15b0582be36c0de17e438466e6aef564d641455e
theryhus <84607552+theryhus@users.noreply.github.com>
2021-11-21 21:14:11 -0600

    Pagina con tarjeticas
    tarjeticas wii

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Blog Post v5.0.6 (https://startbootstrap.com/template/blog-post)
+* Start Bootstrap - Blog Home v5.0.7 (https://startbootstrap.com/template/blog-home)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
